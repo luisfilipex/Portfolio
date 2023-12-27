d33a29158e1b723847df1b501d547baadb76037c
Luis Filipe e Silva <137459500+luisfilipex@users.noreply.github.com>
2023-12-26 21:05:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var form = document.getElementById("myForm");
+  var mensagemDiv = document.querySelector(".mensagem");
+
+  alert("Mensagem enviada com sucesso!");
+
+  form.reset();
+
+  mensagemDiv.innerHTML = "Mensagem enviada com sucesso!";
+  mensagemDiv.style.color = "green";
+});
